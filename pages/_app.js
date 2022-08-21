006c4dcf96de23885a0b69a0827548a6e0dd1c5b
innocentdavid <paulinnocent04@gmail.com>
2022-08-21 11:30:37 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { IoMdMenu } from 'react-icons/io'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <nav className="flex justify-around items-center w-full h-16 bg-black text-white fixed top-0 left-0">
+      <div className="cursor-pointer"><IoMdMenu size="30px" /></div>
+      <div className="">
+        <img src="/BUSD MINER_LOGO 22.png" alt="LOGO" width="60px" />
+      </div>
+      <div className="cursor-pointer font-['Poppins'] font-semibold py-1 px-2 border border-[#ffa500] hover:border-white rounded-full">CONNECT</div>
+    </nav>
+    <div className="mt-16"></div>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
