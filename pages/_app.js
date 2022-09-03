830bc4ee83ae574eb5ed902dd8bcb4e2c84612a0
onoja64 <77271290+onoja64@users.noreply.github.com>
2022-09-03 18:41:12 +0100

    web3 connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,29 @@
-import '../styles/globals.css'
-import { IoMdMenu } from 'react-icons/io'
+import "../styles/globals.css";
+import { IoMdMenu } from "react-icons/io";
+import { ConnectButton } from "web3uikit";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
 function MyApp({ Component, pageProps }) {
-  return (<>
-    <nav className="flex justify-around items-center w-full h-16 bg-black text-white fixed top-0 left-0">
-      <div className="cursor-pointer"><IoMdMenu size="30px" /></div>
-      <div className="">
-        <img src="/BUSD MINER_LOGO 22.png" alt="LOGO" width="60px" />
-      </div>
-      <div className="cursor-pointer font-['Poppins'] font-semibold py-1 px-2 border border-[#ffa500] hover:border-white rounded-full">CONNECT</div>
-    </nav>
-    <div className="mt-16"></div>
-    <Component {...pageProps} />
-  </>)
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <NotificationProvider>
+        <>
+          <nav className="flex justify-around items-center w-full h-16 bg-black text-white fixed top-0 left-0">
+            <div className="cursor-pointer">
+              <IoMdMenu size="30px" />
+            </div>
+            <div className="">
+              <img src="/BUSD MINER_LOGO 22.png" alt="LOGO" width="60px" />
+            </div>
+            <ConnectButton moralisAuth={false} />
+          </nav>
+          <div className="mt-16"></div>
+          <Component {...pageProps} />
+        </>
+      </NotificationProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
