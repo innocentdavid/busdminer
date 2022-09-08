ab46dff049d51e09dcec648c456be35e4b1e90b6
innocentdavid <paulinnocent04@gmail.com>
2022-09-08 11:35:23 +0100

    cxb

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,16 @@
 import "../styles/globals.css";
-import { IoMdMenu } from "react-icons/io";
-import { ConnectButton } from "web3uikit";
 import { MoralisProvider } from "react-moralis";
 import { NotificationProvider } from "web3uikit";
+import { AppProvider } from "../context/AppContext";
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <MoralisProvider initializeOnMount={false}>
       <NotificationProvider>
-        <>
-          <nav className="flex justify-around items-center w-full h-16 bg-black text-white fixed top-0 left-0">
-            <div className="cursor-pointer">
-              <IoMdMenu size="30px" />
-            </div>
-            <div className="">
-              <img src="/BUSD MINER_LOGO 22.png" alt="LOGO" width="60px" />
-            </div>
-            <ConnectButton moralisAuth={false} />
-          </nav>
-          <div className="mt-16"></div>
-          <Component {...pageProps} />
-        </>
+        <AppProvider>
+            <Component {...pageProps} />
+        </AppProvider>
       </NotificationProvider>
     </MoralisProvider>
   );
