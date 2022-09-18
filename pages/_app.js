8404356842caa0e2cdc098fe84a687c9cb47543b
innocentdavid <paulinnocent04@gmail.com>
2022-09-18 16:26:48 +0100

    ksjd

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,15 @@
 import "../styles/globals.css";
-// import { MoralisProvider } from "react-moralis";
 import { NotificationProvider } from "web3uikit";
 import { AppProvider } from "../context/AppContext";
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    // <MoralisProvider initializeOnMount={false}>
       <NotificationProvider>
         <AppProvider>
           <Component {...pageProps} />
         </AppProvider>
       </NotificationProvider>
-    // </MoralisProvider>
   );
 }
 
